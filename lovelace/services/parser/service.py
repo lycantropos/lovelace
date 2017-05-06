95a6f1f71d33fe55425aa7ccf2cda1b1a10dfc1b
ace <azatibrakov@gmail.com>
2017-05-06 19:28:06 +0700

    Renaming: 'section line' -> 'section title'

--- lovelace/services/parser/service.py
+++ lovelace/services/parser/service.py
@@ -6,7 +6,7 @@ from lovelace.utils import lines_generator
 def parse_page_sections_names(content: str
                               ) -> Iterator[str]:
     content_lines = lines_generator(content)
-    sections_lines = filter(is_section_line, content_lines)
+    sections_lines = filter(is_section_title, content_lines)
     for line in sections_lines:
         yield line.strip(' =')
 
@@ -14,17 +14,17 @@ def parse_page_sections_names(content: str
 def parse_page_section_content_lines(section_name: str,
                                      content: str
                                      ) -> Iterator[str]:
-    section_line = f'== {section_name} =='
+    section_title = f'== {section_name} =='
     content_lines = lines_generator(content)
-    next(line for line in content_lines if line == section_line)
+    next(line for line in content_lines if line == section_title)
     for line in content_lines:
-        line_is_section = is_section_line(line)
-        if line_is_section:
+        line_is_section_title = is_section_title(line)
+        if line_is_section_title:
             return
         yield line
 
 
-def is_section_line(line: str) -> bool:
+def is_section_title(line: str) -> bool:
     section_line_head, section_line_tail = '== ', ' =='
     return (line.startswith(section_line_head) and
             line.endswith(section_line_tail))
