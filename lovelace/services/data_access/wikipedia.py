254d32dd2747f61e12367dbe0eccddfae90a321a
ace <azatibrakov@gmail.com>
2017-09-11 17:42:37 +0700

    Setting default values using 'dict.setdefault' instead of '__setitem__'

--- lovelace/services/data_access/wikipedia.py
+++ lovelace/services/data_access/wikipedia.py
@@ -10,8 +10,8 @@ async def query_wikipedia_api(*,
                               session: ClientSession,
                               **params: Dict[str, str]
                               ) -> Dict[str, Any]:
-    params['format'] = 'json'
-    params['action'] = 'query'
+    params.setdefault('format', 'json')
+    params.setdefault('action', 'query')
     async with session.get(API_URL,
                            params=params) as response:
         response_json = await response.json()
